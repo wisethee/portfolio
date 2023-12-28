344ac4a3dd8f023ba5a7a3a70a0ae1cefc213a5d
Marius P <marius.paduraru@gmail.com>
2023-12-28 08:40:19 +0000

    feat: add fixed positioning and width to header, update hero layout and typography, update spacing variables in index.css

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -3,13 +3,15 @@ import "./index.css";
 const Hero = () => {
   return (
     <section className="mp-hero">
-      <div className="col-span-4 sm:col-span-6 sm:col-start-2 md:col-start-4 flex flex-col gap-y-6-12 items-center justify-center">
-        <span className="text-title-large uppercase">Marius P. Paduraru</span>
-        <div className="flex flex-col items-center text-display-large uppercase">
+      <div className="mp-hero__inner">
+        <span className="mp-hero__subtitle mp-title--large">
+          Marius P. Paduraru
+        </span>
+        <div className="mp-hero__title mp-display--large">
           <span>Crafting</span>
           <span>good</span>
-          <span className="text-primary">digital</span>
-          <span className="text-primary">stories</span>
+          <span>digital</span>
+          <span>stories</span>
           <span>with</span>
           <span>code</span>
         </div>
