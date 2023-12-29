992af7359bfc55a01b5229e5c3e55fe2fc521a3c
Marius P <marius.paduraru@gmail.com>
2023-12-29 22:38:39 +0000

    feat: add opacity to about text

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { animated, useInView, useSpring, useScroll } from "@react-spring/web";
 
 import "./index.css";
 
@@ -6,26 +7,48 @@ const defaultParagraph =
   "I'm a web developer who finds beauty in simplicity. My love for coding drives me to craft efficient solutions that highlight the inherent simplicity hidden within every project.";
 
 const About = () => {
-  const containerRef = useRef<HTMLParagraphElement | null>(null);
+  const container = useRef<HTMLDivElement>(null!);
   const [paragraph, setParagraph] = useState<string[]>([]);
 
-  useEffect(() => {
-    const t = defaultParagraph.split(" ");
-    setParagraph(t);
+  const [ref, inView] = useInView({
+    rootMargin: "0% 0%",
+  });
 
-    console.log(paragraph);
+  const { scrollYProgress } = useScroll();
+
+  const springs = useSpring({
+    opacity: inView
+      ? scrollYProgress.to((position) => {
+          console.log(position);
+        })
+      : 0.3,
+  });
+
+  useEffect(() => {
+    const documentHeight = document.body.clientHeight;
+
+    if (container.current) {
+      const start = container.current.offsetTop;
+      const end = container.current.offsetTop + container.current.clientHeight;
+      console.log("a: ", start / documentHeight);
+      console.log("b: ", end / documentHeight);
+    }
+  }, [inView]);
+
+  useEffect(() => {
+    setParagraph(defaultParagraph.split(" "));
   }, []);
 
   return (
     <div id="about" className="mp-about">
-      <div className="mp-about-inner" ref={containerRef}>
+      <div className="mp-about-inner">
         <span className="mp-title-large">About</span>
-        <div className="mp-about-text mp-display-medium">
+        <div className="mp-about-text mp-display-medium" ref={container}>
           {paragraph.map((word, index) => (
-            <span key={index}>
+            <animated.span key={index} ref={ref} style={springs}>
               {word}
               {index !== paragraph.length - 1 ? " " : ""}
-            </span>
+            </animated.span>
           ))}
         </div>
       </div>
