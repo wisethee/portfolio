6284eb4438f05be2d3e02ec436f622a0ef904bd9
Marius P <marius.paduraru@gmail.com>
2023-12-31 19:19:05 +0000

    refactor: about component and fix CSS styling

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,11 +1,5 @@
-import { useEffect, useState } from "react";
-import {
-  SpringValue,
-  animated,
-  useInView,
-  useScroll,
-  useSpring,
-} from "@react-spring/web";
+import { useEffect, useRef, useState } from "react";
+import { animated, useInView, useScroll, useSpring } from "@react-spring/web";
 
 import { useScrollPosition } from "../../hooks";
 
@@ -19,55 +13,31 @@ type WordProps = {
   word: string;
   index: number;
   length: number;
-  scrollYProgress: SpringValue<number>;
   start: number;
   end: number;
 };
 
-const Word = ({
-  word,
-  index,
-  length,
-  scrollYProgress,
-  start,
-  end,
-}: WordProps) => {
+const Word = ({ word, index, length, start, end }: WordProps) => {
   const [ref, inView] = useInView({
-    rootMargin: "-18% 0%",
+    rootMargin: "0% 0%",
   });
+
+  const { scrollYProgress } = useScroll();
 
   const [spring, api] = useSpring(() => ({
     opacity: inView ? 1 : 0,
   }));
 
-  // Calculate the range for this word
-  const scrollRange = end - start;
-  const wordPosition = (index + 1) / length;
-  const wordStart = start + wordPosition * scrollRange;
-
   useEffect(() => {
-    inView && console.log(word);
-  }, [inView]);
-
-  useEffect(() => {
-    api({
+    api.start({
       opacity: inView
         ? scrollYProgress.to((progress: number) => {
-            if (progress < start) {
-              return 0.3;
-            } else if (progress > end) {
-              return 1;
-            } else {
-              const opacity = Math.max(
-                0.3,
-                Math.min(1, (progress - wordStart) / scrollRange + 0.3)
-              );
-              return opacity;
-            }
+            console.log(progress);
+            return 1;
           })
         : 0.3,
     });
-  }, [inView, scrollYProgress, start, end, api, wordStart, scrollRange]);
+  }, [inView, scrollYProgress, api]);
 
   return (
     <animated.span className="mp-about-word" ref={ref} style={spring}>
@@ -79,20 +49,9 @@ const Word = ({
 
 const About = () => {
   const [paragraph, setParagraph] = useState<string[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  const [ref, inView] = useInView();
-  const { scrollYProgress } = useScroll();
+  const ref = useRef<HTMLDivElement>(null!);
 
   const [start, end] = useScrollPosition(ref);
-
-  useEffect(() => {
-    if (!loading && inView) {
-      console.log({ start, end });
-    } else {
-      setLoading(false);
-    }
-  }, [inView, loading, start, end]);
 
   useEffect(() => {
     setParagraph(defaultParagraph.split(" "));
@@ -109,7 +68,6 @@ const About = () => {
               word={word}
               index={index}
               length={paragraph.length}
-              scrollYProgress={scrollYProgress}
               start={start}
               end={end}
             />
