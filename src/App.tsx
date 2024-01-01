c51e101ce0124a18e69ef48c55c765643d0eae52
Marius P <marius.paduraru@gmail.com>
2024-01-01 15:19:43 +0000

    core: update ESLint configuration and add @stylexjs eslint-plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import Header from "./components/header";
-
 import "./app.css";
-import Main from "./components/main";
-import Hero from "./components/hero";
-import Footer from "./components/footer";
-import Resize from "./components/resize";
-import About from "./components/about";
 
 const App = () => {
-  return (
-    <>
-      <Header />
-
-      <Main>
-        <Hero />
-        <About />
-      </Main>
-
-      <Footer />
-
-      <Resize />
-    </>
-  );
+  return <>App.</>;
 };
 
 export default App;
